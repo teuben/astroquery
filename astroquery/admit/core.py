0a5e0dfaac7c96b0ef9dc9afa412274b277145e2
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-02-21 11:11:17 -0500

    update with alma keywords

--- astroquery/admit/core.py
+++ astroquery/admit/core.py
@@ -5,6 +5,9 @@ import os
 import sqlite3
 import pickle
 from astropy.table import Table
+#from astropy import units as u
+#from astropy.time import Time
+
 from ..query import BaseQuery
 from ..utils import commons, async_to_sync
 from ..alma.tapsql import _gen_pos_sql, _gen_str_sql, _gen_numeric_sql,\
@@ -12,6 +15,7 @@ from ..alma.tapsql import _gen_pos_sql, _gen_str_sql, _gen_numeric_sql,\
     _gen_science_sql, _gen_spec_res_sql, ALMA_DATE_FORMAT
 
 __all__ = ['ADMIT', 'ADMITClass']
+
 
 # This mimics the ALMA_FORM_KEYS in alma/core.py.  The assumption here is
 # that there is a web form in front of this.  We don't have it for ADMIT, but
@@ -24,30 +28,29 @@ __all__ = ['ADMIT', 'ADMITClass']
 #
 ADMIT_FORM_KEYS = {
     'Window': {  
-    # we need "spw.id and sources.spw_id" so put spw in two
-        'ALMA ID': ['alma_id','spw.alma_id',_gen_numeric_sql],
-        'Spectral Window': [ 'spw','spw.id', _gen_numeric_sql],
-        'Number of Lines': [ 'nlines','spw.nlines', _gen_numeric_sql],
-        'Number of Sources': [ 'nsources','spw.nsources', _gen_numeric_sql],
-        'Number of Channels': [ 'nchan','spw.nchan', _gen_numeric_sql],
-        'RMS noise': [ 'rms','spw.rms', _gen_numeric_sql],
+        'ALMA ID': ['alma_id','win.alma_id',_gen_numeric_sql],
+        'Spectral Window': [ 'spw','win.id', _gen_numeric_sql],
+        'Number of Lines': [ 'nlines','win.nlines', _gen_numeric_sql],
+        'Number of Sources': [ 'nsources','win.nsources', _gen_numeric_sql],
+        'Number of Channels': [ 'nchan','win.nchan', _gen_numeric_sql],
+        'RMS noise': [ 'rms','win.rms', _gen_numeric_sql],
      },
     'Lines': {
-        'Spectral Window': [ 'spw','lines.spw_id', _gen_numeric_sql],
+        'Spectral Window': [ 'spw','lines.w_id', _gen_numeric_sql],
         'Transition': [ 'transition','lines.transition', _gen_str_sql],
         'Velocity': [ 'velocity','lines.velocity', _gen_numeric_sql],
         # we are not using this
         #'Channels': [ 'chan','lines.chan', _gen_numeric_sql],
      },
     'Sources': {
-        'Spectral Window': [ 'spw','sources.spw_id', _gen_numeric_sql],
+        'Spectral Window': [ 'spw','sources.w_id', _gen_numeric_sql],
         'Line ID': [ 'lines_id','sources.lines_id', _gen_numeric_sql],
         'RA (Degrees)': ['ra', 'sources.ra',  _gen_numeric_sql],
         'Dec (Degrees)': ['dec', 'sources.dec',  _gen_numeric_sql],
         'Flux': ['flux', 'sources.flux',  _gen_numeric_sql],
-        # source.sn is not actually a table column but we can use it as 
+        # source.snr is not actually a table column but we can use it as 
         # a trigger to munge some sql post-facto.
-        'Signale to Noise': ['sn', 'sources.sn',  _gen_numeric_sql],
+        'Signal to Noise Ratio': ['snr', 'sources.snr',  _gen_numeric_sql],
      },
     'Header': { # no science use case
         'Key': ['header_key','header.key',_gen_str_sql],
@@ -60,10 +63,56 @@ ADMIT_FORM_KEYS = {
      },
     'Alma': {
         'Observation': ['obs_id','alma.obs_id',_gen_str_sql],
-        'Source name': ['source_name','alma.target_name',_gen_str_sql],
-        'Source name': ['source_name','alma.target_name',_gen_str_sql],
+        # From here below are just a copy of ALMA_FORM_KEYS without the external wrapper dict.
+        # Position
+        'Source name (astropy Resolver)': ['source_name_resolver',
+                                           'SkyCoord.from_name', _gen_pos_sql],
+        'Source name (ALMA)': ['source_name_alma', 'alma.target_name', _gen_str_sql],
         'RA Dec (Sexagesimal)': ['ra_dec', 'alma.s_ra, alma.s_dec', _gen_pos_sql],
+        'Galactic (Degrees)': ['galactic', 'alma.gal_longitude, alma.gal_latitude',
+                               _gen_pos_sql],
+        'Angular resolution (arcsec)': ['spatial_resolution',
+                                        'alma.spatial_resolution', _gen_numeric_sql],
+        'Largest angular scale (arcsec)': ['spatial_scale_max',
+                                           'alma.spatial_scale_max', _gen_numeric_sql],
+        'Field of view (arcsec)': ['fov', 'alma.s_fov', _gen_numeric_sql],
+        # Energy
         'Frequency (GHz)': ['frequency', 'alma.frequency', _gen_numeric_sql],
+        'Bandwidth (Hz)': ['bandwidth', 'alma.bandwidth', _gen_numeric_sql],
+        'Spectral resolution (KHz)': ['spectral_resolution',
+                                      'alma.em_resolution', _gen_spec_res_sql],
+        'Band': ['band_list', 'alma.band_list', _gen_band_list_sql],
+        # Time
+        'Observation date': ['start_date', 'alma.t_min', _gen_datetime_sql],
+        'Integration time (s)': ['integration_time', 'alma.t_exptime',
+                                 _gen_numeric_sql],
+        # Polarization
+        'Polarisation type (Single, Dual, Full)': ['polarisation_type',
+                                                   'alma.pol_states', _gen_pol_sql],
+        # Observation
+        'Line sensitivity (10 km/s) (mJy/beam)': ['line_sensitivity',
+                                                  'alma.sensitivity_10kms',
+                                                  _gen_numeric_sql],
+        'Continuum sensitivity (mJy/beam)': ['continuum_sensitivity',
+                                             'alma.cont_sensitivity_bandwidth',
+                                             _gen_numeric_sql],
+        'Water vapour (mm)': ['water_vapour', 'alma.pvw', _gen_numeric_sql],
+        # Project
+        'Project code': ['project_code', 'alma.proposal_id', _gen_str_sql],
+        'Project title': ['project_title', 'alma.obs_title', _gen_str_sql],
+        'PI name': ['pi_name', 'alma.obs_creator_name', _gen_str_sql],
+        'Proposal authors': ['proposal_authors', 'alma.proposal_authors', _gen_str_sql],
+        'Project abstract': ['project_abstract', 'alma.proposal_abstract', _gen_str_sql],
+        'Publication count': ['publication_count', 'alma.NA', _gen_str_sql],
+        'Science keyword': ['science_keyword', 'alma.science_keyword', _gen_str_sql],
+       # Publication'
+        'Bibcode': ['bibcode', 'alma.bib_reference', _gen_str_sql],
+        'Title': ['pub_title', 'alma.pub_title', _gen_str_sql],
+        'First author': ['first_author', 'alma.first_author', _gen_str_sql],
+        'Authors': ['authors', 'alma.authors', _gen_str_sql],
+        # this may need special handling? or person does search on pub_abstract="*YSO*" 
+        'Abstract': ['pub_abstract', 'alma.pub_abstract', _gen_str_sql],
+        'Year': ['publication_year', 'alma.pub_year', _gen_numeric_sql],
      },
 }
 
@@ -132,7 +181,7 @@ class ADMITClass(BaseQuery):
         """
         if self.c == None:
             print("database not open yet")
-        for t in ["header", "alma", "spw", "lines", "sources", "cont"]:
+        for t in ["header", "alma", "win", "lines", "sources", "cont"]:
             print("%-10s: %d entries" % (t,len(self.sql("SELECT id from %s" % t))))
 
     def sql(self, command):
@@ -206,7 +255,7 @@ class ADMITClass(BaseQuery):
         """
         print("admit.db contains tables that work with the ALMA table")
         print("   alma:      a placeholder for the big alma table")
-        print("   spw:       all SPW's are stored here, with links back to the alma reference")
+        print("   win:       all SPW's are stored here, with links back to the alma reference")
         print("   lines:     all lines detected in the SPW's")
         print("   sources:   all sources detected in each ADMIT LineCube")
 
@@ -234,11 +283,11 @@ def find_data_url(result_page):
 
 
 # maybe we will have a different sql param for inner join
-def _gen_sql2(payload,sql = 'select * spw,lines,sources'):
+def _gen_sql2(payload,sql = 'select * win,lines,sources'):
     pass
 
 def _gen_sql(payload):
-    sql = 'select * spw,lines,sources'
+    sql = 'select * win,lines,sources'
     where = ''
     if payload:
         for constraint in payload:
@@ -248,11 +297,17 @@ def _gen_sql(payload):
                         # use the value and the second entry in attrib which
                         # is the new name of the column
                         val = payload[constraint]
-                        attrib_where = attrib[2](attrib[1], val)
-                        # replace sources.sn with '( sources.flux / spw.rms )'
-                        # to compute signal to noise
-                        if 'sources.sn' in attrib_where:
-                            attrib_where = attrib_where.replace('sources.sn','( sources.flux / spw.rms )')
+                        # see astroquery/alma/core.py
+                        if constraint == 'alma.em_resolution':
+                            # em_resolution does not require any transformation
+                            attrib_where = _gen_numeric_sql(constraint, val)
+                        else:
+                            attrib_where = attrib[2](attrib[1], val)
+                        # ADMIT virtual keyword
+                        #  Replace sources.snr with '( sources.flux / win.rms )'
+                        #  to compute signal to noise
+                        if 'sources.snr' in attrib_where:
+                            attrib_where = attrib_where.replace('sources.snr','( sources.flux / win.rms )')
                         if attrib_where:
                             if where:
                                 where += ' AND '
